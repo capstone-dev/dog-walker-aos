d23ad4bce8bf786a2cc32c2509ae013a76e30d35
kyj6118@naver.com <kyj6118@naver.com>
2019-05-03 14:03:35 +0900

    log인 부분 추가

---
+++ app/src/main/java/ajou/ac/kr/teaming/activity/Login/LoginMainActivity.java
@@ -0,0 +1,15 @@
+package ajou.ac.kr.teaming.activity.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ajou.ac.kr.teaming.R;
+
+public class LoginMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+    }
+}
