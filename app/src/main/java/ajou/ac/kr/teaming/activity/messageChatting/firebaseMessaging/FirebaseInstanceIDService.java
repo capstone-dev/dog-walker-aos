7f4c1f22bfac680026b2e9cbe2a13d47617e41a3
gihong884 <rlghd7414@gmail.com>
2019-05-15 00:26:21 +0900

    메시지 기능 추가

--- app/src/main/java/ajou/ac/kr/teaming/activity/messageChatting/firebaseMessaging/FirebaseInstanceIDService.java
+++ app/src/main/java/ajou/ac/kr/teaming/activity/messageChatting/firebaseMessaging/FirebaseInstanceIDService.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+
 public class FirebaseInstanceIDService extends FirebaseMessagingService {
 
     @Override
