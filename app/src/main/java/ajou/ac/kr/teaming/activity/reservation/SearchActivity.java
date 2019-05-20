46f83292bbaad8b409aca053659b1dc1c643b1ab
goyejin <kyj6118@naver.com>
2019-05-20 17:51:20 +0900

    Dogwalker 검색부분 추가

--- app/src/main/java/ajou/ac/kr/teaming/activity/reservation/SearchActivity.java
+++ app/src/main/java/ajou/ac/kr/teaming/activity/reservation/SearchActivity.java
@@ -1,9 +1,13 @@
 package ajou.ac.kr.teaming.activity.reservation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ajou.ac.kr.teaming.R;
+import ajou.ac.kr.teaming.activity.gps.GpsMainActivity;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -11,5 +15,16 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Button RealTimeButton = findViewById(R.id.SearchButton);
+        RealTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent searchresultIntent=new Intent(SearchActivity.this, SearchResultActivity.class);
+                SearchActivity.this.startActivity(searchresultIntent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/ajou/ac/kr/teaming/vo/DogwalkerVo.java
@@ -0,0 +1,18 @@
+package ajou.ac.kr.teaming.vo;
+
+import android.media.Image;
+
+public class DogwalkerVo {
+
+    private String UserID;
+    private String UserName;
+    private String UserEmail;
+    private String UserGender;
+    private String UserPhoneNumber;
+    private String UserBigcity;
+    private String UserTime;
+    private String UserSmallcity;
+    private String Userdate;
+    private Image User;
+}
+
