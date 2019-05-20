0538754dc4239e2165a84f8346289c53a15efdc7
goyejin <kyj6118@naver.com>
2019-05-20 17:58:53 +0900

    Dogwalker 등록부분 추가

--- app/src/main/java/ajou/ac/kr/teaming/activity/login/MyPageActivity.java
+++ app/src/main/java/ajou/ac/kr/teaming/activity/login/MyPageActivity.java
@@ -1,12 +1,20 @@
 package ajou.ac.kr.teaming.activity.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import ajou.ac.kr.teaming.R;
+import ajou.ac.kr.teaming.activity.MainActivity;
+import ajou.ac.kr.teaming.activity.gps.GpsMainActivity;
+import ajou.ac.kr.teaming.activity.reservation.ReservationActivity;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    Button DogwalkerButton;
 
 
     @Override
@@ -14,5 +22,20 @@ public class MyPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
+       DogwalkerButton = (Button) findViewById(R.id.DogwalkerButton);
+
+       DogwalkerButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent DogwalkerIntent=new Intent(MyPageActivity.this, MainActivity.class);
+               MyPageActivity.this.startActivity(DogwalkerIntent);
+           }
+       });
+
+
+
     }
+
+
+
 }
