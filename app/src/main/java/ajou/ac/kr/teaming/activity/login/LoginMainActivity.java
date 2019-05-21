bd3fccac7c517a601b9cc43d76025af9e1dbf450
goyejin <kyj6118@naver.com>
2019-05-21 15:55:57 +0900

    login 등록 부분 구현

--- app/src/main/java/ajou/ac/kr/teaming/activity/login/LoginMainActivity.java
+++ app/src/main/java/ajou/ac/kr/teaming/activity/login/LoginMainActivity.java
@@ -39,95 +39,85 @@ public class LoginMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login_main);
 
 
-        idText = (EditText)findViewById(R.id.idText);
-        passwordText = (EditText)findViewById(R.id.passwordText);
-        loginButton = (Button)findViewById(R.id.loginButton);
-        registerButton = (TextView)findViewById(R.id.registerButton);
-        loginService =ServiceBuilder.create(LoginService.class);
+        idText = (EditText) findViewById(R.id.idText);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (TextView) findViewById(R.id.registerButton);
+        loginService = ServiceBuilder.create(LoginService.class);
 
 
-       /* loginButton.setOnClickListener(new View.OnClickListener(){
+        loginButton.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View view) {
 
-                String userid=idText.getText().toString();
-                String userpassword=passwordText.getText().toString();
+                String userid = idText.getText().toString();
+                String userpassword = passwordText.getText().toString();
 
                 //Validate form
-                if(ValidateLogin(userid,userpassword)){
+                if (ValidateLogin(userid, userpassword)) {
 
                     //dologin
-                    doLogin(userid,userpassword);
+                    doLogin(userid, userpassword);
                 }
 
 
-
             }
-        });*/
+        });
 
 
-
-
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent loginIntent=new Intent(LoginMainActivity.this,MainActivity.class);
+                Intent loginIntent = new Intent(LoginMainActivity.this, MainActivity.class);
                 LoginMainActivity.this.startActivity(loginIntent);
             }
         });
-
-
-
     }
 
 
+        private boolean ValidateLogin (String userid, String userpassword){
+            if (userid == null || userid.trim().isEmpty()) {
+                Toast.makeText(this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (userpassword == null || userpassword.trim().isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            return true;
 
-
-   /* private boolean ValidateLogin(String userid,String userpassword){
-        if (userid==null ||userid.trim().isEmpty()){
-            Toast.makeText(this,"아이디를 입력하세요",Toast.LENGTH_SHORT).show();
-            return false;
         }
-        if (userpassword==null ||userpassword.trim().isEmpty()){
-            Toast.makeText(this,"비밀번호를 입력하세요",Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        return true;
 
-    }
+        private void doLogin ( final String userid, final String userpassword){
 
-    private void doLogin(final String userid,final String userpassword){
-
-        Call<RegisterVO> call=loginService.DoLogin(userid,userpassword);
-        call.enqueue(new Callback<RegisterVO>() {
-            @Override
-            public void onResponse(Call<RegisterVO> call, Response<RegisterVO> response) {
-                if(response.isSuccessful()){
-                    RegisterVO registerVO=response.body();
+            Call<RegisterVO> call = loginService.DoLogin(userid, userpassword);
+            call.enqueue(new Callback<RegisterVO>() {
+                @Override
+                public void onResponse(Call<RegisterVO> call, Response<RegisterVO> response) {
+                    if (response.isSuccessful()) {
+                        RegisterVO registerVO = response.body();
 
 
-                    if (registerVO != null) {
-                        Log.d("TEST", registerVO.getUserID());
-                        Log.d("TEST", registerVO.getUserPassword());
+                        if (registerVO != null) {
+                            Log.d("TEST", registerVO.getUserID());
+                            Log.d("TEST", registerVO.getUserPassword());
 
+                        }
+
+                        Intent intent = new Intent(LoginMainActivity.this, MainActivity.class);
+                        intent.putExtra("registerVO", registerVO);
+                        startActivity(intent);
                     }
 
-                  Intent intent=new Intent(LoginMainActivity.this,MainActivity.class);
-                    intent.putExtra("registerVO", registerVO);
-                    startActivity(intent);
                 }
 
-            }
+                @Override
+                public void onFailure(Call<RegisterVO> call, Throwable t) {
+                    Toast.makeText(LoginMainActivity.this, "Error", Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onFailure(Call<RegisterVO> call, Throwable t) {
-                Toast.makeText(LoginMainActivity.this,"Error",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-    }*/
-}
+                }
+            });
+        }
+    }
