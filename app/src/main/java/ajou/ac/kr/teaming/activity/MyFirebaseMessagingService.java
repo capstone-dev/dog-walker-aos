aed2dab0076f768a57ecdb9fede2f81109f63554
gihong884 <rlghd7414@gmail.com>
2019-05-30 20:24:17 +0900

    푸쉬 알림

--- app/src/main/java/ajou/ac/kr/teaming/activity/MyFirebaseMessagingService.java
+++ app/src/main/java/ajou/ac/kr/teaming/activity/MyFirebaseMessagingService.java
@@ -1,45 +1,28 @@
-/*
-package ajou.ac.kr.teaming.activity.firebase;
+package ajou.ac.kr.teaming.activity;
 
 import android.app.NotificationChannel;
-
 import android.app.NotificationManager;
-
 import android.app.PendingIntent;
-
 import android.content.Context;
 import android.content.Intent;
-
 import android.media.RingtoneManager;
-
 import android.net.Uri;
-
 import android.os.Build;
-
-import android.os.Vibrator;
-
 import android.support.v4.app.NotificationCompat;
-
 import android.util.Log;
 
-
-
 import com.google.firebase.messaging.FirebaseMessagingService;
-
 import com.google.firebase.messaging.RemoteMessage;
 
 import ajou.ac.kr.teaming.R;
-import ajou.ac.kr.teaming.activity.MainActivity;
 
-
-public class FireBaseMessagingService extends FirebaseMessagingService {
-
-    private static final String TAG = "MyFirebaseMsgService";
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MsgService";
 
     @Override
-
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
+        // TODO(developer): Handle FCM messages here.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         if (remoteMessage.getData().size() > 0) {
@@ -50,9 +33,11 @@ public class FireBaseMessagingService extends FirebaseMessagingService {
                 handleNow();
             }
         }
+
         if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
         }
     }
 
@@ -60,36 +45,33 @@ public class FireBaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "Short lived task is done.");
     }
 
-    private void sendNotification(String messageBody) {
+    private void sendNotification(String messageTitle, String messageBody) {
+
         Intent intent = new Intent(this, MainActivity.class);
 
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         String channelId = getString(R.string.default_notification_channel_id);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
                         .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("FCM Message")
+                        .setContentTitle(messageTitle)
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
                         .setContentIntent(pendingIntent);
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        */
-/*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String channelName = getString(R.string.default_notification_channel_name);
             NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
             notificationManager.createNotificationChannel(channel);
-        }*//*
-
+        }
         notificationManager.notify(0, notificationBuilder.build());
     }
 }
-*/
