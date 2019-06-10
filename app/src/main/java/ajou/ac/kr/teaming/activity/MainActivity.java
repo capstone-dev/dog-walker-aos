73bc6adbfa7ecdb510919199ea8eacc1b73ada34
gihong884 <rlghd7414@gmail.com>
2019-06-10 19:33:29 +0900

    메인 화면 수정

--- app/src/main/java/ajou/ac/kr/teaming/activity/MainActivity.java
+++ app/src/main/java/ajou/ac/kr/teaming/activity/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,24 +20,40 @@ import android.view.Menu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import ajou.ac.kr.teaming.R;
 import ajou.ac.kr.teaming.activity.gps.DogwalkerGpsActivity;
 import ajou.ac.kr.teaming.activity.gps.GpsMainActivity;
 import ajou.ac.kr.teaming.activity.login.DogwalkerRegister;
 import ajou.ac.kr.teaming.activity.login.LoginMainActivity;
 import ajou.ac.kr.teaming.activity.login.MyPetActivity;
+import ajou.ac.kr.teaming.activity.messageChatting.MessageChattingMainActivity;
 import ajou.ac.kr.teaming.activity.messageChatting.messageList.MessageListMainActivity;
+import ajou.ac.kr.teaming.activity.myService.MyServiceAdapter;
 import ajou.ac.kr.teaming.activity.myService.MyServiceMainActivity;
 import ajou.ac.kr.teaming.activity.reservation.ReservationActivity;
 import ajou.ac.kr.teaming.activity.userCommunity.UserCommunityMainActivity;
 import ajou.ac.kr.teaming.service.gallery.GalleryService;
+import ajou.ac.kr.teaming.service.gps.GpsRealTimeDogwalkerService;
 import ajou.ac.kr.teaming.service.gps.GpsService;
 import ajou.ac.kr.teaming.service.history.HistoryService;
+import ajou.ac.kr.teaming.service.myService.MyServiceService;
 import ajou.ac.kr.teaming.service.sample.SampleService;
 import ajou.ac.kr.teaming.service.common.ServiceBuilder;
+import ajou.ac.kr.teaming.service.servicePay.ServicePayService;
+import ajou.ac.kr.teaming.vo.DogwalkerListVO;
 import ajou.ac.kr.teaming.vo.RegisterVO;
+import ajou.ac.kr.teaming.vo.ServiceVO;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener ,
+        MyServiceAdapter.OnMessageItemClickListener, MyServiceAdapter.OnDeleteItemClickListener,MyServiceAdapter.OnMyServiceClickListener{
 
     private static final String TAG = "MainActivity";
     private SampleService sampleService = ServiceBuilder.create(SampleService.class);
@@ -43,6 +62,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private GpsService gpsService = ServiceBuilder.create(GpsService.class);
     private RegisterVO registerVO;
     private FloatingActionButton messageButton;
+
+
+    private ServicePayService servicePayService = ServiceBuilder.create(ServicePayService.class);
+    private MyServiceService myServiceService = ServiceBuilder.create(MyServiceService.class);
+    private GpsRealTimeDogwalkerService gpsRealTimeDogwalkerService = ServiceBuilder.create(GpsRealTimeDogwalkerService.class);
+    private RecyclerView myServiceView;
+    private RecyclerView dogwalkerServiceView;
+    private MyServiceAdapter myServiceAdapter;
+    private MyServiceAdapter dogwalkerServiceAdapter;
+    private TextView myService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +117,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         View textView=(View)findViewById(R.id.main_layout_1);
         messageButton=(FloatingActionButton)textView.findViewById(R.id.fab);
         messageButton.setOnClickListener(this);
+
+        //contentview 연결
+        View contentView=(View)textView.findViewById(R.id.connect_content_main);
+
+        myServiceView = contentView.findViewById(R.id.my_service_list_view);
+        dogwalkerServiceView = contentView.findViewById(R.id.dogwalker_my_service_list_view);
+        myServiceView.setLayoutManager(new LinearLayoutManager(this));
+        dogwalkerServiceView.setLayoutManager(new LinearLayoutManager(this));
+
+        myService=contentView.findViewById(R.id.service_list_form);
+        myService.setText(registerVO.getUserID()+"님의 서비스 리스트");
+
+        myServiceAdapter = new MyServiceAdapter(this::deleteMyServiceEvent, this::contactMessageEvent,this::clickMyServiceEvent);
+        dogwalkerServiceAdapter = new MyServiceAdapter(this::deleteMyServiceEvent, this::contactMessageEvent,this::clickMyServiceEvent);
+        myServiceView.setAdapter(myServiceAdapter);
+        dogwalkerServiceView.setAdapter(dogwalkerServiceAdapter);
+        setMyServiceList();
+        setDogwalkerServiceList();
 
     }
 
@@ -187,9 +234,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
 
 
-
-
-
         if (intent != null) {
             startActivity(intent);
 
@@ -209,5 +253,169 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 service.putExtra("RegisterVO",registerVO);
                 startActivity(service);
         }
+    }
+
+    @Override
+    public void deleteMyServiceEvent(View view, ServiceVO serviceVO) {
+        Call<ResponseBody> request = myServiceService.deleteService(Integer.toString(serviceVO.getId()));
+        request.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                // 성공시
+                if (response.isSuccessful()) {
+                    ResponseBody body = response.body();
+                    //테스트 확인 log값
+                    if (body != null) {
+                        Log.d("TEST", body.toString());
+                    }
+                }
+                Log.d("TEST", "삭제 성공 ");
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                //실패시
+                Log.d("TEST", "삭제 실패" + t.toString());
+            }
+        });
+        updateDogwalkerRealTime(serviceVO);
+        //현재 페이지에서 메인 usercommunitymain으로 새로고침 하면서 이동
+        setMyServiceList();
+        setDogwalkerServiceList();
+    }
+
+    @Override
+    public void contactMessageEvent(View view, ServiceVO serviceVO) {
+        Intent intent = new Intent(MainActivity.this, MessageChattingMainActivity.class);
+        intent.putExtra("RegisterVO", registerVO);
+        intent.putExtra("ServiceVO", serviceVO);
+        intent.putExtra("activityName", "나의서비스");
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void clickMyServiceEvent(View view, ServiceVO serviceVO) {
+        //실시간 도그워커 사용자가 신청중으로 다시 바꿈
+
+        HashMap<String, Object> inputThread=new HashMap<>();
+
+        inputThread.put("DogwalkerID",serviceVO.getUser_DogwalkerID());
+        inputThread.put("selected","0");
+
+        Call<DogwalkerListVO> request = gpsRealTimeDogwalkerService.deleteMyService(inputThread);
+        request.enqueue(new Callback<DogwalkerListVO>() {
+            @Override
+            public void onResponse(Call<DogwalkerListVO> call, Response<DogwalkerListVO> response) {
+                // 성공시
+                if (response.isSuccessful()) {
+                    DogwalkerListVO dogwalkerListVOs = response.body();
+                    //테스트 확인 log값
+                    if (dogwalkerListVOs != null) {
+                        Log.d("TEST", dogwalkerListVOs.getDogwalkerID());
+                    }
+                }
+                Log.d("TEST", "수정 성공 ");
+            }
+
+            @Override
+            public void onFailure(Call<DogwalkerListVO> call, Throwable t) {
+                //실패시
+                Log.d("TEST", "수정 실패" + t.getMessage());
+            }
+        });
+        setMyServiceList();
+    }
+
+    private void setMyServiceList() {
+        myServiceAdapter.checkService("my");
+        myServiceAdapter.updateService();
+        Call<List<ServiceVO>> request = servicePayService.getUserService(registerVO.getUserID());
+        request.enqueue(new Callback<List<ServiceVO>>() {
+            @Override
+            public void onResponse(Call<List<ServiceVO>> call, Response<List<ServiceVO>> response) {
+
+                List<ServiceVO> serviceVOS = response.body();
+                // 성공시
+                ArrayList<ServiceVO> serviceVOArrayList = new ArrayList<>();
+                if (serviceVOS != null) {
+                    for (ServiceVO serviceVO : serviceVOS) {
+                        //서버로 부터 읽어드린 게시글 리스트를 전부 adapter 에 저장
+                        serviceVOArrayList.add(serviceVO);
+                        Log.d("TEST", "나의 서비스 가져오기! " + serviceVO.getServiceLocation());
+                    }
+                    myServiceAdapter.addService(serviceVOArrayList);
+                }
+                Log.d("TEST", "게시글 통신 성공");
+            }
+
+            @Override
+            public void onFailure(Call<List<ServiceVO>> call, Throwable t) {
+                //실패시
+                Log.d("TEST", "게시글 통신 실패");
+            }
+        });
+    }
+
+    private void setDogwalkerServiceList() {
+        dogwalkerServiceAdapter.checkService("dogwalker");
+        dogwalkerServiceAdapter.updateService();
+        Call<List<ServiceVO>> request = servicePayService.getDogwalkerService(registerVO.getUserID());
+        request.enqueue(new Callback<List<ServiceVO>>() {
+            @Override
+            public void onResponse(Call<List<ServiceVO>> call, Response<List<ServiceVO>> response) {
+
+                List<ServiceVO> serviceVOS = response.body();
+                // 성공시
+                ArrayList<ServiceVO> serviceVOArrayList = new ArrayList<>();
+                if (serviceVOS != null) {
+                    for (ServiceVO serviceVO : serviceVOS) {
+                        //서버로 부터 읽어드린 게시글 리스트를 전부 adapter 에 저장
+                        serviceVOArrayList.add(serviceVO);
+                        Log.d("TEST", "나의 서비스 가져오기! " + serviceVO.getServiceLocation());
+                    }
+                    dogwalkerServiceAdapter.addService(serviceVOArrayList);
+                }
+                Log.d("TEST", "게시글 통신 성공");
+            }
+
+            @Override
+            public void onFailure(Call<List<ServiceVO>> call, Throwable t) {
+                //실패시
+                Log.d("TEST", "게시글 통신 실패");
+            }
+        });
+    }
+
+    public void updateDogwalkerRealTime(ServiceVO serviceVO) {
+        //실시간 도그워커 사용자가 신청중으로 다시 바꿈
+
+        HashMap<String, Object> inputThread=new HashMap<>();
+
+        inputThread.put("DogwalkerID",serviceVO.getUser_DogwalkerID());
+        inputThread.put("selected","0");
+
+        Call<DogwalkerListVO> request = gpsRealTimeDogwalkerService.deleteMyService(inputThread);
+        request.enqueue(new Callback<DogwalkerListVO>() {
+            @Override
+            public void onResponse(Call<DogwalkerListVO> call, Response<DogwalkerListVO> response) {
+                // 성공시
+                if (response.isSuccessful()) {
+                    DogwalkerListVO dogwalkerListVOs = response.body();
+                    //테스트 확인 log값
+                    if (dogwalkerListVOs != null) {
+                        Log.d("TEST", dogwalkerListVOs.getDogwalkerID());
+                    }
+                }
+                Log.d("TEST", "수정 성공 ");
+            }
+
+            @Override
+            public void onFailure(Call<DogwalkerListVO> call, Throwable t) {
+                //실패시
+                Log.d("TEST", "수정 실패" + t.getMessage());
+            }
+        });
+        setMyServiceList();
     }
 }
